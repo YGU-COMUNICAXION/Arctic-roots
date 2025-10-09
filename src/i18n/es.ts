630905834ff52248de01757691df46829c5253e0
Jonathan Alexis Bello Lopez <jonathan.bellofg@gmail.com>
2025-10-09 01:58:25 -0600

    add: highlights sections

--- src/i18n/es.ts
+++ src/i18n/es.ts
@@ -19,17 +19,17 @@ const es = {
   home: {
     highlights: {
       hero: {
-        title: "Islandia y Groenlandia: la tierra del hielo y fuego",
+        title: "Islandia y Groenlandia: la<br/> tierra del hielo y fuego",
         paragraphs: [
-          "Imagina descubrir un país que te ofrece el furor de los campos de lava y, al mismo tiempo, murallas de hielo y cascadas surrealistas.",
+          "Imagina descubrir un país que te ofrece el furor de los <b>campos de lava</b> y, al mismo tiempo, <b>murallas de hielo</b> y <b>cascadas surrealistas</b>.",
           "Islandia es un destino para espíritus entusiastas por admirar la naturaleza y que tengan capacidad de asombro; no importa cuánta expectativa traigan, esta tierra no despide a nadie igual que como llegó.",
         ],
         backgroundAlt: "Erupción de lava sobre un paisaje volcánico islandés",
       },
       discovery: {
         title:
-          "Es un lugar en el que tanto niños como adultos se sienten en un museo viviente",
-        description: "Y será aún más fascinante si son:",
+          "Es un lugar en el que tanto niños como adultos se sienten en un museo viviente y será aún más fascinante si son:",
+        description: "",
         items: [
           { label: "Fotógrafos", icon: "photographers" },
           { label: "Amantes de la naturaleza", icon: "natureLovers" },
@@ -37,7 +37,10 @@ const es = {
             label: "Viajeros que buscan turismo sostenible",
             icon: "sustainableTravelers",
           },
-          { label: "Personas con deseo de ver auroras boreales", icon: "auroraChasers" },
+          {
+            label: "Personas con deseo de ver auroras boreales",
+            icon: "auroraChasers",
+          },
           { label: "Viajeros solitarios", icon: "soloTravelers" },
           { label: "Creadores de contenido", icon: "contentCreators" },
           { label: "Parejas en luna de miel", icon: "honeymooners" },
